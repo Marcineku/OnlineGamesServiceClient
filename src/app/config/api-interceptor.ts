b06420d5064550f8713a23d00323665cda0585e8
Marcineku <37746326+Marcineku@users.noreply.github.com>
2019-01-07 16:21:31 +0100

    Added login image

--- src/app/config/api-interceptor.ts
+++ src/app/config/api-interceptor.ts
@@ -4,8 +4,9 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
+  /*http://192.168.0.248:8080/*/
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://192.168.0.248:8080/${req.url}` });
+    const apiReq = req.clone({ url: `http://localhost:8080${req.url}` });
     return next.handle(apiReq);
   }
 }
