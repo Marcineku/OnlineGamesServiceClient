be429eb766952fad59cd8c927d6382edd7213327
Marcineku <37746326+Marcineku@users.noreply.github.com>
2019-01-28 11:05:51 +0100

    Added logout on 402 & skip header for api interceptor

--- src/app/http-interceptors/api-interceptor.ts
+++ src/app/http-interceptors/api-interceptor.ts
@@ -2,22 +2,38 @@ import {Injectable} from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, of, throwError} from 'rxjs';
 import {concatMap, delay, retryWhen} from 'rxjs/operators';
+import {AuthService} from '../services/auth.service';
 
 // 'http://localhost:8080/';
 // 'http://192.168.0.248:8080/';
+
+export const InterceptorSkipHeader = 'X-Skip-Interceptor';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
   private apiUrl = 'http://localhost:8080/';
 
+  constructor(private auth: AuthService) {
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.headers.has(InterceptorSkipHeader)) {
+      const headers = req.headers.delete(InterceptorSkipHeader);
+      return next.handle(req.clone({headers}));
+    }
+
     return next.handle(req.clone({url: this.apiUrl + req.url})).pipe(
       retryWhen(errors => errors.pipe(
         concatMap((err, i) => {
-          if (err.status === 0) {
-            if (i < 2) {
-              return of(err);
-            }
+          switch (err.status) {
+            case 0:
+              if (i < 2) {
+                return of(err);
+              }
+              break;
+            case 401:
+              this.auth.logout();
+              break;
           }
           return throwError(err);
         }),
